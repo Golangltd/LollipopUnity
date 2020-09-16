2ee67a190900e5098c32c8f3abffdd0162269fdb
byteedu <1528884535@qq.com>
2020-09-16 13:03:45 +0800

    add sc scrip

--- Assets/Scenes/LollipopUnity.cs
+++ Assets/Scenes/LollipopUnity.cs
@@ -9,6 +9,7 @@ public class LollipopUnity : MonoBehaviour
     {
         // 加载音效、头像管理、设置FrameRate、切换后台后游戏多长时间重启
         // 设备ID 保存
+        Debug.Log("Golang语言社区 LollipopUnity 1.0.X");
     }
 
     void Update()
