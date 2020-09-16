92b201783533591ea7a9c7f0da9ed805f68421fb
byteedu <1528884535@qq.com>
2020-09-16 18:30:38 +0800

    add sc scrip

--- Assets/Scenes/LollipopUnity.cs
+++ Assets/Scenes/LollipopUnity.cs
@@ -1,19 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XLua;
+using System.IO;  //引入这个空间是为了输入路径用
 
 public class LollipopUnity : MonoBehaviour
 {
-
+    //创建Lua虚拟机
+    LuaEnv luaenv = new LuaEnv();
+    // Start is called before the first frame update
     void Start()
     {
-        // 加载音效、头像管理、设置FrameRate、切换后台后游戏多长时间重启
-        // 设备ID 保存
-        Debug.Log("Golang语言社区 LollipopUnity 1.0.X");
+        luaenv.AddLoader(CustomLoader);
+        luaenv.DoString("require 'LollipopUnity'");
+
+        GetGlobalData();
+
+    
     }
 
-    void Update()
+    #region 1.C#访问Lua中的全局基本数据类型
+    void GetGlobalData()
     {
-        
+        double a = luaenv.Global.Get<double>("a");
+        print("a = " + a);//a = 100.1
+        string str = luaenv.Global.Get<string>("str");
+        print("str = " + str);//str = hahaha
+        bool isDie = luaenv.Global.Get<bool>("isDie");
+        print("isDie = " + isDie);//isDie = False
+
+        a = 10;
+        a = luaenv.Global.Get<double>("a");
+        print(a);//a = 100.1  说明在这种直接获取的情况下不能改变Lua脚本中的变量值，只能获取
+    }
+    #endregion
+
+
+    private byte[] CustomLoader(ref string filepath)      //这个是在网上查的，可以记下来作为一个API来使用，这个函数返回一个自定义路径
+    {
+        //重定向  可以在AB包里去读取 也可以指定 本地的一个路径
+        string str = Application.dataPath + "/Scenes/" + filepath + ".lua";
+        Debug.Log(str);
+        if (File.Exists(str))
+            return File.ReadAllBytes(str);
+        return null;
     }
 }
