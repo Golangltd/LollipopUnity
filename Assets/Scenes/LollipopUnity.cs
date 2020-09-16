5dad1ec2157814ddb7eb887631e69f793a8c5836
byteedu <1528884535@qq.com>
2020-09-16 12:58:12 +0800

    add sc scrip

---
+++ Assets/Scenes/LollipopUnity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LollipopUnity : MonoBehaviour
+{
+
+    void Start()
+    {
+        // 加载音效、头像管理、设置FrameRate、切换后台后游戏多长时间重启
+        // 设备ID 保存
+    }
+
+    void Update()
+    {
+        
+    }
+}
